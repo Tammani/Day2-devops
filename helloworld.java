42c9d83fa8b1c6d2b58018d57bff1b51d90f5fc6
Tammani <107013963+Tammani@users.noreply.github.com>
2025-11-10 08:49:35 +0530

    Change class name and greeting message

--- helloworld.java
+++ helloworld.java
@@ -1,9 +1,9 @@
-public class HelloWorld {
+public class helloworld {
     
     // Your program begins with a call to main()
     public static void main(String[] args)
     {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World");
+        System.out.println("Hello, Tammani");
     }
 }
